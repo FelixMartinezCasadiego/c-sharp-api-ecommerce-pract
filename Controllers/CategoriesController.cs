9c1a4cc58550329362130dd955cac7920f4a3791
Felix Gabriel Martinez Casadiego <felix.martinez@auraxtechnology.com>
2026-01-07 15:42:37 +0100

    feat: Add GetCategory endpoint and update repository method to return nullable Category

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -25,5 +25,22 @@ namespace ApiEcommerce.Controllers
             }
             return Ok(categoriesDto);
         }
+
+        [HttpGet("{id:int}", Name = "GetCategory")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetCategory(int id)
+        {
+            var category = _categoryRepository.GetCategory(id);
+            if (category == null)
+            {
+                return NotFound($"Category with id {id} not found.");
+            }
+
+            var categoryDto = _mapper.Map<CategoryDto>(category);
+            return Ok(categoryDto);
+        }
     }
 }

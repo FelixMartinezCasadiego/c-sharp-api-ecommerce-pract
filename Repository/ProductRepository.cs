4cd82cb8fd91cc3b96bb004dd5f404ad1b1d93d9
Felix Gabriel Martinez Casadiego <felix.martinez@auraxtechnology.com>
2026-01-07 17:14:48 +0100

    feat: Implement ProductRepository with CRUD operations and search functionality

---
+++ Repository/ProductRepository.cs
@@ -0,0 +1,122 @@
+using ApiEcommerce.Models;
+using ApiEcommerce.Repository.IRepository;
+
+namespace ApiEcommerce.Repository;
+
+public class ProductRepository(ApplicationDbContext db) : IProductRepository
+{
+    private readonly ApplicationDbContext _db = db;
+    public bool BuyProduct(string name, int quantity)
+    {
+        if(string.IsNullOrWhiteSpace(name) || quantity <= 0)
+        {
+            return false;
+        }
+        var product = _db.Products.FirstOrDefault(p => p.Name.ToLower().Trim() == name.ToLower().Trim());
+        if(product == null || product.StockQuantity < quantity)
+        {
+            return false;
+        }
+        product.StockQuantity -= quantity;
+        return Save();
+    }
+
+    public bool CreateProduct(Product product)
+    {
+        if(product == null)
+        {
+            return false;
+        }
+        product.CreationDate = DateTime.Now;
+        product.UpdateDate = DateTime.Now;
+        _db.Products.Add(product);
+        return Save();
+    }
+
+    public bool DeleteProduct(Product product)
+    {
+        if(product == null)
+        {
+            return false;
+        }
+        _db.Products.Remove(product);
+        return Save();
+    }
+
+    public Product? GetProduct(int id)
+    {
+        if(id <= 0)
+        {
+            return null;
+        }
+        return _db.Products.FirstOrDefault(p => p.ProductId == id);
+    }
+
+    public ICollection<Product> GetProducts()
+    {
+        return _db.Products.OrderBy(p => p.Name).ToList();
+    }
+
+    public ICollection<Product> GetProductsForCategory(int categoryId)
+    {
+        if(categoryId <= 0)
+        {
+            return [];
+        }
+        
+        return _db.Products.Where(p => p.CategoryId == categoryId).OrderBy(p => p.Name).ToList();
+    }
+
+    public bool ProductExists(int id)
+    {
+        if(id <= 0)
+        {
+            return false;
+        }
+        return _db.Products.Any(p => p.ProductId == id);
+    }
+
+    public bool ProductExists(string name)
+    {
+        if(string.IsNullOrWhiteSpace(name))
+        {
+            return false;
+        }
+        return _db.Products.Any(p => p.Name.ToLower().Trim() == name.ToLower().Trim());
+    }
+
+    public bool Save()
+    {
+        return _db.SaveChanges() >= 0; // Returns true if one or more entities were changed
+    }
+
+    public ICollection<Product> SearchProduct(string name)
+    {
+        // if(string.IsNullOrWhiteSpace(name))
+        // {
+        //     return [];
+        // }
+        // return _db.Products
+        //     .Where(p => p.Name.ToLower().Trim().Contains(name.ToLower().Trim()))
+        //     .OrderBy(p => p.Name)
+        //     .ToList();
+
+        IQueryable<Product> query = _db.Products;
+        if(!string.IsNullOrWhiteSpace(name))
+        {
+            query = query.Where(p => p.Name.ToLower().Trim() == name.ToLower().Trim());
+        }
+        return query.OrderBy(p => p.Name).ToList();
+    }
+
+    public bool UpdateProduct(Product product)
+    {
+        if(product == null)
+        {
+            return false;
+        }
+        product.UpdateDate = DateTime.Now;
+        _db.Products.Update(product);
+        return Save();
+    }
+}
